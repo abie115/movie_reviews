4f41128bb2d9ac858ebd22baa5874f64d6bfd5a2
Aldona <donicczka@gmail.com>
2015-04-14 01:13:27 +0200

    maly blad v2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -35,7 +35,7 @@ class MoviesController < ApplicationController
 
     respond_to do |format|
       if @movie.save
-        format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
+        format.html { redirect_to @movie, notice: 'Movie review was successfully created.' }
         format.json { render :show, status: :created, location: @movie }
       else
         format.html { render :new }
@@ -49,7 +49,7 @@ class MoviesController < ApplicationController
   def update
     respond_to do |format|
       if @movie.update(movie_params)
-        format.html { redirect_to @movie, notice: 'Movie was successfully updated.' }
+        format.html { redirect_to @movie, notice: 'Movie review was successfully updated.' }
         format.json { render :show, status: :ok, location: @movie }
       else
         format.html { render :edit }
@@ -63,7 +63,7 @@ class MoviesController < ApplicationController
   def destroy
     @movie.destroy
     respond_to do |format|
-      format.html { redirect_to movies_url, notice: 'Movie was successfully destroyed.' }
+      format.html { redirect_to movies_url, notice: 'Movie review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
